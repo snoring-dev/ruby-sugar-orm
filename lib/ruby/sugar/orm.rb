8393b8e361dec274b573e75f42f61cd8cbb0cbb0
snoring-dev <jemmoudi.med@proton.me>
2023-02-21 16:01:06 +0100

    init(lib): start working on sugar orm library

---
+++ lib/ruby/sugar/orm.rb
@@ -0,0 +1,10 @@
+require "ruby/sugar/orm/version"
+
+module Ruby
+  module Sugar
+    module Orm
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/sugar/orm/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Sugar
+    module Orm
+      VERSION = "0.1.0"
+    end
+  end
+end
